28978352f1ead639bd0a38cdbed48e33f62e6a76
Andrey Blandón Mora <andreybm31@disroot.org>
2018-12-29 13:25:18 -0600

    add method calculate daily messages

--- counter.py
+++ counter.py
@@ -1,15 +1,46 @@
 from os import environ
 from datetime import datetime, timedelta
 
+from peewee import fn
+
 from models.tables import User, Message
 
-GROUP_ID = environ['GROUP_ID']
+GROUP_ID = int(environ['GROUP_ID'])
+
+
+def msg_daily(bot, update):
+
+    chat_id = update.message.chat_id
+
+    today = datetime.today() - timedelta(hours=6)
+    yesterday = today - timedelta(days=1)
+
+    total_daily = Message.select().where(Message.date > yesterday,
+                                         Message.date < today)
+    msg = 'Reporte últimas 24hrs\n\n'\
+          f'Mensajes\n └ Totales: {total_daily.count()}\n'
+    top_ten = (Message
+               .select(Message.user, fn.count(Message.user)
+                       .alias('num_messages'))
+               .where(Message.date > yesterday, Message.date < today)
+               .group_by(Message.user)
+               .limit(10))
+    tops = ''
+    count = 1
+    for i in top_ten:
+        tops += f'TOP {count}: \n'\
+                f' ├alias: @{i.user.username} \n'\
+                f' ├nombre: {i.user.first_name}\n'\
+                f' └enviados: {i.num_messages}\n'
+        count += 1
+
+    bot.send_message(chat_id=chat_id, text=msg+tops)
 
 
 def counter(bot, update):
     chat_id = update.message.chat_id
 
-    if chat_id == int(GROUP_ID):
+    if chat_id == GROUP_ID:
 
         message = update.message
         user_id = message.from_user.id
